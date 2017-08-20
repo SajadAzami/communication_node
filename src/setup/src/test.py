a181552f3ec8a4fa7bd2c6a50255ece1dd920d0d
taherahmadi <14taher@gmail.com>
2017-08-20 22:25:00 +0430

    fixes some bug, still problems with catkin_make

--- src/setup/src/test.py
+++ src/setup/src/test.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-import rospy;
-import sys;
-sys.path.append("/home/user/workspaces/communication_node/src/sosvr_gazebo_plugins/scripts");
-from getInfo import  GetInfo;
+import rospy
+import sys
 
+sys.path.append("/home/user/workspaces/communication_node/src/gazebo_plugins/scripts")
+from getInfo import GetInfo
 
-if (__name__ == "__main__") :
-    rospy.init_node("test_node");
-    a = GetInfo();
-    print (a.Request(robot1="asphalt_plane",robot2="asphalt_plane_1")[1]);
-    print ("this was the distance that you expected");
+if __name__ == "__main__":
+    rospy.init_node("test_node")
+    a = GetInfo()
+    print a.request(robot1="asphalt_plane", robot2="asphalt_plane_1")[1]
+    print "this was the distance that you expected"
