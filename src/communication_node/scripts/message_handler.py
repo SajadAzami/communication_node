aff9b69cd43b80dd0546e7f3fac6472ad8dfdfae
Hossein76 <hoseingohari76@gmail.com>
2017-11-02 21:35:22 +0330

    added instructions and added launchfile for message_handler

--- src/communication_node/scripts/message_handler.py
+++ src/communication_node/scripts/message_handler.py
@@ -19,9 +19,10 @@ import sys
 from time import gmtime,strftime
 import rospy
 from communication_node.msg import Data_Position
-
 from environment_information import get_object_distance
 from propagation_models import one_slope_model_checker
+
+debuger_mode=False
 information_logger=None
 rate=None
 
@@ -53,31 +54,37 @@ def callback(data):
                 rate.sleep()
             i=0
             print "communication is possible"
-            information_logger.write(data.command+"".join(["-" for k in range(0,8-len(data.command))]))
-            information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
-            information_logger.write(data.destination+"".join(["-" for k in range(0,16-len(data.destination))]))
-            information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
-            information_logger.write("message sent"+"\n")
+            if debuger_mode==True :
+               information_logger.write(data.command+"".join(["-" for k in range(0,8-len(data.command))]))
+               information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
+               information_logger.write(data.destination+"".join(["-" for k in range(0,16-len(data.destination))]))
+               information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
+               information_logger.write("message sent"+"\n")
 
         else:
             # TODO, ignore the message, send feedback
-            information_logger.write(data.command+"".join(["-" for k in range(0,8-len(data.command))]))
-            information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
-            information_logger.write(data.destination+"".join(["-" for k in range(0,16-len(data.destination))]))
-            information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
-            information_logger.write("failed"+"\n")
+            if debuger_mode==True :
+              information_logger.write(data.command+"".join(["-" for k in range(0,8-len(data.command))]))
+              information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
+              information_logger.write(data.destination+"".join(["-" for k in range(0,16-len(data.destination))]))
+              information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
+              information_logger.write("failed"+"\n")
             print "communication is not possible"
 
 
 def listener():
     global information_logger
     global rate
+    global debuger_mode
     rospy.init_node('communication_node_message_handler')
     rate=rospy.Rate(10)
-    information_logger =  open("./results.txt", "a")
-    information_logger.write("\n ###################### \n ###################### \n")
-    information_logger.write("\n This is the result of test on "+strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " GMT time \n")
-    information_logger.write("Number--Source-----Destination-----Distance----------Outcome\n");
+    debuger_mode=rospy.get_param("debuger_mode",default=False)
+    if debuger_mode==True :
+         log_file=rospy.get_param("log_file",default="results.txt")
+         information_logger =  open("./"+log_file, "a")
+         information_logger.write("\n ###################### \n ###################### \n")
+         information_logger.write("\n This is the result of test on "+strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " GMT time \n")
+         information_logger.write("Number--Source-----Destination-----Distance----------Outcome\n");
     rospy.Subscriber("/message_server", Data_Position, callback,queue_size=50)
     signal.signal(signal.SIGINT, on_exit)
     signal.signal(signal.SIGTERM, on_exit)
