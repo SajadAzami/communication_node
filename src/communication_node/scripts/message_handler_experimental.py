940a39a05798f71ab01ebbf9efcbd0d197837548
Hossein76 <hoseingohari76@gmail.com>
2017-11-30 22:19:17 +0330

    addding class for generic service handeling

--- src/communication_node/scripts/message_handler_experimental.py
+++ src/communication_node/scripts/message_handler_experimental.py
@@ -27,6 +27,7 @@ from propagation_models import one_slope_model_checker
 debuger_mode=False
 information_logger=None
 rate=None
+message_handlers_list=[];
 
 def on_exit(*args):
     global information_logger
@@ -36,170 +37,64 @@ def on_exit(*args):
          information_logger.close()
     sys.exit(0)
 
-def callback_MtA(data):
-    global information_logger
-    # TODO handle for different message types
-    # TODO prop_model = data.prop_model
-    print("new MtA received")
-    prop_model = '1sm'
-    if prop_model == '1sm':
-        # distance = get_object_distance("pioneer3at", "Dumpster")
-        distance = get_object_distance(data.source,data.destination)
-        print (type(distance),"MtA")
-        while (distance==None):
-            distance = get_object_distance(data.destination, data.source)
-        result = one_slope_model_checker(distance=distance)
-        if result:
-            message_publisher = rospy.Publisher(data.destination + '/inbox_MtA', Data_MtA, queue_size=10)
-            i=0
-            while not ( rospy.is_shutdown() or i>1):
-                message_publisher.publish(data)
-                i+=1
-                rate.sleep()
-            i=0
-            print "communication is possible"
-            if debuger_mode==True :
-               information_logger.write("MtA"+"".join(["-" for k in range(0,5)]))
-               information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
-               information_logger.write(data.destination+"".join(["-" for k in range(0,20-len(data.destination))]))
-               information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
-               information_logger.write("message sent"+"\n")
+class message_handle:
+    def _init_(self,subs_topic="",pub_topic="",data_type=None,tag="",alt_type=None):
+        self.subs_topic=subs_topic;
+        self.pub_topic="";
+        self.data_type=data_type;
+        self.alt_type=alt_type;
+        self.tag=tag;
+        self.subscriber=rospy.Subscriber(self.subs_topic,self.data_type, self.callback_function,queue_size=50);
+        self.message_publisher=None;
+    def callback_function(self,data):
+            global information_logger
+            # TODO handle for different message types
+            # TODO prop_model = data.prop_model
+            print("new "+tag+" received")
+            prop_model = '1sm'
+            if prop_model == '1sm':
+                # distance = get_object_distance("pioneer3at", "Dumpster")
+                distance = get_object_distance(data.destination, data.source)
+                print (type(distance),tag)
+                while (distance==None):
+                    distance = get_object_distance(data.destination, data.source)
+                result = one_slope_model_checker(distance=distance)
+                if result:
+                    if (self.alt_type!=None):
+                       self.message_publisher = rospy.Publisher(data.source + self.pub_topic, self.alt_type, queue_size=10)
+                   else:
+                       self.message_publisher = rospy.Publisher(data.source + self.pub_topic, self.data_type, queue_size=10)
+                    i=0
+                    while not ( rospy.is_shutdown() or i>1):
+                        self.message_publisher.publish(data.data)
+                        i+=1
+                        rate.sleep()
+                    i=0
+                    print "communication is possible"
+                    if debuger_mode==True :
+                       information_logger.write(self.tag+"".join(["-" for k in range(0,11-len(self.tag))]))
+                       information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
+                       information_logger.write(data.destination+"".join(["-" for k in range(0,20-len(data.destination))]))
+                       information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
+                       information_logger.write("message sent"+"\n")
 
-        else:
-            # TODO, ignore the message, send feedback
-            if debuger_mode==True :
-              information_logger.write("MtA"+"".join(["-" for k in range(0,5)]))
-              information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
-              information_logger.write(data.destination+"".join(["-" for k in range(0,20-len(data.destination))]))
-              information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
-              information_logger.write("failed"+"\n")
-            print "communication is not possible"
+                else:
+                    # TODO, ignore the message, send feedback
+                    if debuger_mode==True :
+                      information_logger.write(self.tag+"".join(["-" for k in range(0,11-len(self.tag))]))
+                      information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
+                      information_logger.write(data.destination+"".join(["-" for k in range(0,20-len(data.destination))]))
+                      information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
+                      information_logger.write("failed"+"\n")
+                    print "communication is not possible"
 
-
-def callback_Odom(data):
-    global information_logger
-    # TODO handle for different message types
-    # TODO prop_model = data.prop_model
-    print("new odom received")
-    prop_model = '1sm'
-    if prop_model == '1sm':
-        # distance = get_object_distance("pioneer3at", "Dumpster")
-        distance = get_object_distance(data.destination, data.source)
-        print (type(distance),"odom")
-        while (distance==None):
-            distance = get_object_distance(data.destination, data.source)
-        result = one_slope_model_checker(distance=distance)
-        if result:
-            message_publisher = rospy.Publisher(data.destination + '/inbox_Odom', Data_Odom, queue_size=10)
-            i=0
-            while not ( rospy.is_shutdown() or i>1):
-                message_publisher.publish(data)
-                i+=1
-                rate.sleep()
-            i=0
-            print "communication is possible"
-            if debuger_mode==True :
-               information_logger.write("Odom"+"".join(["-" for k in range(0,4)]))
-               information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
-               information_logger.write(data.destination+"".join(["-" for k in range(0,20-len(data.destination))]))
-               information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
-               information_logger.write("message sent"+"\n")
-
-        else:
-            # TODO, ignore the message, send feedback
-            if debuger_mode==True :
-              information_logger.write("Odom"+"".join(["-" for k in range(0,4)]))
-              information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
-              information_logger.write(data.destination+"".join(["-" for k in range(0,20-len(data.destination))]))
-              information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
-              information_logger.write("failed"+"\n")
-            print "communication is not possible"
-
-
-def callback_AtM(data):
-    global information_logger
-    # TODO handle for different message types
-    # TODO prop_model = data.prop_model
-    print("new AtM received")
-    prop_model = '1sm'
-    if prop_model == '1sm':
-        # distance = get_object_distance("pioneer3at", "Dumpster")
-        distance = get_object_distance(data.destination, data.source)
-        print (type(distance),"AtM")
-        while (distance==None):
-            distance = get_object_distance(data.destination, data.source)
-        result = one_slope_model_checker(distance=distance)
-        if result:
-            message_publisher = rospy.Publisher(data.destination + '/inbox_AtM', Data_AtM, queue_size=10)
-            i=0
-            while not ( rospy.is_shutdown() or i>1):
-                message_publisher.publish(data)
-                i+=1
-                rate.sleep()
-            i=0
-            print "communication is possible"
-            if debuger_mode==True :
-               information_logger.write("AtM"+"".join(["-" for k in range(0,5)]))
-               information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
-               information_logger.write(data.destination+"".join(["-" for k in range(0,20-len(data.destination))]))
-               information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
-               information_logger.write("message sent"+"\n")
-
-        else:
-            # TODO, ignore the message, send feedback
-            if debuger_mode==True :
-              information_logger.write("AtM"+"".join(["-" for k in range(0,5)]))
-              information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
-              information_logger.write(data.destination+"".join(["-" for k in range(0,20-len(data.destination))]))
-              information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
-              information_logger.write("failed"+"\n")
-            print "communication is not possible"
-
-
-def callback_map(data):
-    global information_logger
-    # TODO handle for different message types
-    # TODO prop_model = data.prop_model
-    print("new map received")
-    prop_model = '1sm'
-    if prop_model == '1sm':
-        # distance = get_object_distance("pioneer3at", "Dumpster")
-        distance = get_object_distance(data.destination, data.source)
-        print (type(distance),"map")
-        while (distance==None):
-            distance = get_object_distance(data.destination, data.source)
-        result = one_slope_model_checker(distance=distance)
-        if result:
-            message_publisher = rospy.Publisher(data.source + '/g_map', OccupancyGrid, queue_size=10)
-            i=0
-            while not ( rospy.is_shutdown() or i>1):
-                message_publisher.publish(data.data)
-                i+=1
-                rate.sleep()
-            i=0
-            print "communication is possible"
-            if debuger_mode==True :
-               information_logger.write("map"+"".join(["-" for k in range(0,5)]))
-               information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
-               information_logger.write(data.destination+"".join(["-" for k in range(0,20-len(data.destination))]))
-               information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
-               information_logger.write("message sent"+"\n")
-
-        else:
-            # TODO, ignore the message, send feedback
-            if debuger_mode==True :
-              information_logger.write("map"+"".join(["-" for k in range(0,5)]))
-              information_logger.write(data.source+"".join(["-" for k in range(0,11-len(data.source))]))
-              information_logger.write(data.destination+"".join(["-" for k in range(0,20-len(data.destination))]))
-              information_logger.write(str(distance)+"".join(["-" for k in range(0,18-len(str(distance)))]))
-              information_logger.write("failed"+"\n")
-            print "communication is not possible"
 
 
 def listener():
     global information_logger
     global rate
     global debuger_mode
+    global message_handlers_list;
     rospy.init_node('communication_node_message_handler')
     rate=rospy.Rate(10)
     debuger_mode=rospy.get_param("debuger_mode",default=False)
@@ -210,11 +105,10 @@ def listener():
          information_logger =  open("/home/user/project_franchesco/communication_node/test_results/"+log_file+"/"+log_file+".log", "a")
          information_logger.write("\n ###################### \n ###################### \n")
          information_logger.write("\n This is the result of test on "+strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " GMT time \n")
-         information_logger.write("Type----Source-----Destination---------Distance----------Outcome\n");
-    rospy.Subscriber("/message_server_MtA", Data_MtA, callback_MtA,queue_size=50)
-    rospy.Subscriber("/message_server_AtM", Data_AtM, callback_AtM,queue_size=50)
-    rospy.Subscriber("/message_server_map", Data_Map, callback_map,queue_size=50)
-    rospy.Subscriber("/message_server_Odom", Data_Odom, callback_Odom,queue_size=50)
+         information_logger.write("Type-------Source-----Destination---------Distance----------Outcome\n");
+    message_list=[["/message_server_MtA","/inbox_MtA",Data_MtA,"MtA"],["/message_server_AtM","/inbox_AtM",Data_AtM,"AtM"],["/message_server_map","/g_map",Data_Map,"map",alt_type=OccupancyGrid],["/message_server_Odom","/inbox_Odom",Data_Odom,"odom"]];
+    for i in range (0,len(message_list)):
+        message_handlers_list.append(message_handle(message_list[i][0],message_list[i][1],message_list[i][2],message_list[i][3]));
     signal.signal(signal.SIGINT, on_exit)
     signal.signal(signal.SIGTERM, on_exit)
     rospy.spin()
